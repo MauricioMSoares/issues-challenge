61cd47054be6d5aa78cfb44435fbd78abbc24d85
Mauricio Massayuki <99891691+MauricioMSoares@users.noreply.github.com>
2024-03-09 18:41:38 -0300

    feat: added put and delete routes

--- api/src/routes/issuesRoutes.js
+++ api/src/routes/issuesRoutes.js
@@ -6,5 +6,7 @@ const routes = express.Router();
 routes.get("/issues", IssueController.listAll);
 routes.get("/issues/:id", IssueController.findById);
 routes.post("/issues", IssueController.create);
+routes.put("/issues", IssueController.updateById);
+routes.delete("/issues", IssueController.deleteById);
 
 export default routes;
